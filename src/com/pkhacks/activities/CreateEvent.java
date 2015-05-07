701602fe3ff863599e5163a0dd02d84f7d4ea5d6
saqibkhalil91 <saqibkahlil91@gmail.com>
2015-05-07 20:52:10 +0500

    Start Date & End Date for creating new event is handled

--- src/com/pkhacks/activities/CreateEvent.java
+++ src/com/pkhacks/activities/CreateEvent.java
@@ -8,12 +8,6 @@ import com.parse.ParseUser;
 import com.parse.SignUpCallback;
 import com.pkhacks.adopters.EventAdopter;
 
-
-
-
-
-
-
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.DatePickerDialog;
@@ -27,66 +21,55 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-public class CreateEvent extends Activity implements OnClickListener{
-	EditText eventName,city,startDate,endDate;
+public class CreateEvent extends Activity implements OnClickListener {
+	EditText eventName, city, startDate, endDate;
 	Button CreateEvent;
 	private DatePicker datePickerStartDate;
-	private ImageView startDateImg;
-	private final int DATE_DIALOG_ID = 999;
-	private final int DATE_DIALOG = 99;
+	private ImageView startDateImg, endDateImg;
+	static final int DATE_DIALOG_ID = 999;
+	static final int DATE_DIALOG = 99;
 	private int year;
 	private int month;
 	private int day;
-	int sum1=0;
+	private boolean flag_checkDatePicker = false;
+	int sum1 = 0;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_create_event);
 		setViews();
 		setCurrentDateOnView();
-		addListenerOnButton();
+		
 	}
-	private void setViews()
-	{
-		eventName =(EditText)findViewById(R.id.edtEventName);
-		city= (EditText)findViewById(R.id.edtCity);
-		startDate=(EditText)findViewById(R.id.edtStartDate);
-		endDate= (EditText)findViewById(R.id.edtEndDate);
-		CreateEvent=(Button)findViewById(R.id.btnCreateEvent);
+
+	private void setViews() {
+		eventName = (EditText) findViewById(R.id.edtEventName);
+		city = (EditText) findViewById(R.id.edtCity);
+		startDate = (EditText) findViewById(R.id.edtStartDate);
+		endDate = (EditText) findViewById(R.id.edtEndDate);
+		CreateEvent = (Button) findViewById(R.id.btnCreateEvent);
 		CreateEvent.setOnClickListener(this);
-	}
-	public void addListenerOnButton() {
-
 		startDateImg = (ImageView) findViewById(R.id.btnStartDate);
-
-		startDateImg.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				
-				showDialog(DATE_DIALOG_ID);
-				
-
-			}
-
-		});
-
+		startDateImg.setOnClickListener(this);
+		endDateImg = (ImageView) findViewById(R.id.btnEndDate);
+		endDateImg.setOnClickListener(this);
 	}
+
 	public void setCurrentDateOnView() {
 
-		//tvDisplayDate = (EditText) findViewById(R.id.tvDate);
+		// tvDisplayDate = (EditText) findViewById(R.id.tvDate);
 		datePickerStartDate = (DatePicker) findViewById(R.id.dpStartDate);
 
-		
 		final Calendar c = Calendar.getInstance();
 		year = c.get(Calendar.YEAR);
 		month = c.get(Calendar.MONTH);
 		day = c.get(Calendar.DAY_OF_MONTH);
 
-
 		datePickerStartDate.init(year, month, day, null);
 
 	}
+
 	private DatePickerDialog.OnDateSetListener datePickerListener = new DatePickerDialog.OnDateSetListener() {
 
 		// when dialog box is closed, below method will be called.
@@ -95,35 +78,45 @@ public class CreateEvent extends Activity implements OnClickListener{
 			year = selectedYear;
 			month = selectedMonth;
 			day = selectedDay;
-			String fstDatepickerAns = new StringBuilder().append(day).append(month+1).append(year).toString();
-			calculations1(fstDatepickerAns);
+			String fstDatepickerAns = new StringBuilder().append(day)
+					.append(month + 1).append(year).toString();
+			dateCalculations(fstDatepickerAns);
 			// set selected date into textview
-			startDate.setText(new StringBuilder().append(month + 1)
-					.append("-").append(day).append("-").append(year)
-					.append(" "));
-
+			if (flag_checkDatePicker) {
+				startDate.setText(new StringBuilder().append(month + 1)
+						.append("-").append(day).append("-").append(year)
+						.append(" "));
+			} else {
+				endDate.setText(new StringBuilder().append(month + 1)
+						.append("-").append(day).append("-").append(year)
+						.append(" "));
+			}
 			// set selected date into datepicker also
 			datePickerStartDate.init(year, month, day, null);
 
 		}
 	};
-	public void calculations1(String a)
-	{
-		int sum = 0;char c;int bb=0;
-		for(int i=0; i<a.length();i++){
-			c= a.charAt(i);
-			bb =Character.getNumericValue(c);
-			sum = sum+bb;
-		
+
+	public void dateCalculations(String a) {
+		int sum = 0;
+		char c;
+		int bb = 0;
+		for (int i = 0; i < a.length(); i++) {
+			c = a.charAt(i);
+			bb = Character.getNumericValue(c);
+			sum = sum + bb;
+
 		}
 		sum1 = sum;
 	}
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.create_event, menu);
 		return true;
 	}
+
 	@Override
 	protected Dialog onCreateDialog(int id) {
 		switch (id) {
@@ -131,76 +124,79 @@ public class CreateEvent extends Activity implements OnClickListener{
 			// set date picker as current date
 			return new DatePickerDialog(this, datePickerListener, year, month,
 					day);
-			
-		
+		case DATE_DIALOG:
+			// set date picker as current date
+			return new DatePickerDialog(this, datePickerListener, year, month,
+					day);
+
 		}
 		return null;
 	}
+
+	@SuppressWarnings("deprecation")
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
-		int key= v.getId();
+		int key = v.getId();
 		switch (key) {
 		case R.id.btnStartDate:
-			
+			showDialog(DATE_DIALOG_ID);
+			flag_checkDatePicker = true;
+
+			break;
+		case R.id.btnEndDate:
+			showDialog(DATE_DIALOG_ID);
+			flag_checkDatePicker = false;
+
 			break;
 		case R.id.btnCreateEvent:
-		
 
 			// Retrieve the text entered from the EditText
 			String getEventName = eventName.getText().toString();
 			String getCity = city.getText().toString();
 			String getStartDate = startDate.getText().toString();
 			String getEndDate = endDate.getText().toString();
-			
+
 			// Force user to fill up the form
-			if (getEventName.equals("") && getCity.equals("") && getStartDate.equals("") && getEndDate.equals("")) {
+			if (getEventName.equals("") && getCity.equals("")
+					&& getStartDate.equals("") && getEndDate.equals("")) {
 				Toast.makeText(getApplicationContext(),
-						"Please complete the sign up form",
-						Toast.LENGTH_LONG).show();
+						"Please complete the sign up form", Toast.LENGTH_LONG)
+						.show();
 
 			} else {
 				// Save new user data into Parse.com Data Storage
-				
+
 				ParseObject Event = new ParseObject("Event");
 				Event.put("event_name", getEventName);
 				Event.put("City", getCity);
 				Event.put("start_date", getStartDate);
 				Event.put("end_date", getEndDate);
-				//Event.saveInBackground();
-			//	Event.saveInBackground(new EventAdopter(a, resLocal, pkevents));
-		/*	
-		  ParseUser user = new ParseUser();
-				user.setUsername(getEventName);
-				user.setPassword(getCity);
-				user.setUsername(getStartDate);
-				user.setPassword(getEndDate);
-		 
-				Event.signUpInBackground(new SignUpCallback() {
-					public void done(ParseException e) {
-						if (e == null) {
-							// Show a simple Toast message upon successful registration
-							Toast.makeText(getApplicationContext(),
-									"Successfully Signed up, please log in.",
-									Toast.LENGTH_LONG).show();
-						} else {
-							Toast.makeText(getApplicationContext(),
-									"Sign up Error", Toast.LENGTH_LONG)
-									.show();
-						}
-					}
-				});*/
+				// Event.saveInBackground();
+				// Event.saveInBackground(new EventAdopter(a, resLocal,
+				// pkevents));
+				/*
+				 * ParseUser user = new ParseUser();
+				 * user.setUsername(getEventName); user.setPassword(getCity);
+				 * user.setUsername(getStartDate); user.setPassword(getEndDate);
+				 * 
+				 * Event.signUpInBackground(new SignUpCallback() { public void
+				 * done(ParseException e) { if (e == null) { // Show a simple
+				 * Toast message upon successful registration
+				 * Toast.makeText(getApplicationContext(),
+				 * "Successfully Signed up, please log in.",
+				 * Toast.LENGTH_LONG).show(); } else {
+				 * Toast.makeText(getApplicationContext(), "Sign up Error",
+				 * Toast.LENGTH_LONG) .show(); } } });
+				 */
 			}
 
-		
-			
 			break;
 
 		default:
 			break;
 		}
-		
-		
+
 	}
 
 }
